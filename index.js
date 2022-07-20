a8baab5ce287c6cb1bbb193d07b1e79f742fbcda
Wildrama <rambo1bc@hotmail.com>
2022-07-20 10:33:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const categoriasRoutes = require('./routes/categorias')
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/SomosAlpinasDataBase',{
+  await mongoose.connect('mongodb://localhost:27017/SomosAlpinasDataBase10',{
     useNewUrlParser :true,
     useUnifiedTopology:true,
   });
