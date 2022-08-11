206de7ca31b8c7a7f3bd90e05019cfba32bd6518
Wildrama <rambo1bc@hotmail.com>
2022-08-11 14:30:37 -0300

    118

--- public/solicitudAxios.js
+++ public/solicitudAxios.js
@@ -5,8 +5,21 @@ const axiosActualizarCategoria = async ()=>{
     try {
         const res = await axios.post(`/administrador/adm-categorias/${idCategoriaDOM}/nuevo-nombre`,{nombre : nombreInput.value})
      
-       
-    
+        if(res){
+            btnActualizarCategoria.classList.remove('btn-outline-primary');
+            btnActualizarCategoria.classList.add('btn-success');
+            btnActualizarCategoria.innerHTML = "Nombre actualizado";
+
+
+            setTimeout(function(){
+                btnActualizarCategoria.classList.remove('btn-success');
+                btnActualizarCategoria.classList.add('btn-outline-success');
+                btnActualizarCategoria.innerHTML = "Actualizar nombre";
+            }, 1500);
+        }
+
+        
+     
     } catch (error) {
         console.log(error)
     }
